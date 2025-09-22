0dd2c2fdbba5d39ad3f8674f361564eaca191980
jowi-dev <joey8williams@gmail.com>
2025-09-22 08:47:11 -0400

    sync commit

--- nvim/lua/languages.lua
+++ nvim/lua/languages.lua
@@ -1,9 +1,9 @@
 
--- Elixir LSP configuration using Expert
-vim.lsp.config('expert', {
-  cmd = { 'expert' },
+-- Elixir LSP configuration using elixir-ls
+vim.lsp.config('elixirls', {
+  cmd = { '/Users/jowi/.local/share/mise/installs/elixir-ls/0.29.3/language_server.sh' },
   root_markers = { 'mix.exs', '.git' },
   filetypes = { 'elixir', 'eelixir', 'heex' },
 })
 
-vim.lsp.enable 'expert'
+vim.lsp.enable 'elixirls'
